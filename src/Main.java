330c9dfa2ddee4287ed0d03144e4a881fcb610a8
Vital-stude <110226252+Vital-stude@users.noreply.github.com>
2022-08-21 13:49:29 +0300

    МАССИВ 1/0

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,7 @@ public class Main {
 
         for (int i = 0; i < 3; i++) {
             System.out.print(arr[i] + ", " + " ");}
-        System.out.println("   ");
-            for (int i = 2; i >= 0; i--) {
+            for (int i = 3-1; i >= 0; i--) {
                 System.out.print(arr[i] + ", " + " ");
         }
         System.out.println(" ");
